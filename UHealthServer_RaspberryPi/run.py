9d44f2524751e96a3eea3e38ff7675c0b26adee8
hs_kim@ee.knu.ac.kr <hs_kim@ee.knu.ac.kr>
2015-09-05 02:35:46 +0000

    apply to paspberry pi

--- UHealthServer_RaspberryPi/run.py
+++ UHealthServer_RaspberryPi/run.py
@@ -3,12 +3,12 @@ import sys
 
 if __name__ == "__main__":
 	ProjectJar = "Test.jar"
-	classpath = "libs/californium-core-1.0.0-SNAPSHOT.jar:libs/element-connector-1.0.0-SNAPSHOT.jar:libs/jdom-2.0.5.jar"
+	classpath = "libs/californium-core-1.0.0-SNAPSHOT.jar:libs/element-connector-1.0.0-SNAPSHOT.jar:libs/jdom-2.0.5.jar:libs/hapi-hl7overhttp-2.2.jar:libs/hapi-structures-v26-2.2.jar:libs/hapi-base-2.2.jar:libs/log4j-1.2.17.jar:libs/slf4j-log4j12-1.6.6.jar:libs/slf4j-api-1.6.6.jar"
 
 	if sys.argv[1] == "c":
 		print("Compile program")
 
-		src = "src/com/ain/coapserver/coapresources/*.java src/com/ain/coapserver/*.java src/com/ain/coapserver/elements/*.java"
+		src = "src/com/ain/coapserver/coapresources/*.java src/com/ain/coapserver/*.java src/com/ain/coapserver/elements/*.java src/com/ain/ihepcd/transaction/pcd01/*.java"
 		os.system("javac -classpath " + classpath +" -d bin " +src)
 		os.system("jar -cvf " + ProjectJar +" -C bin/ .")
 
